50c964e8633dd28261d77bf3be7a52c9eec6ba5b
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-30 19:05:33 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,22 @@
+class Artist
+    attr_accessor :name, :song, :genre
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def songs
+
+    end
+
+    def genres
+
+    end
+
+    def self.all
+        @@all
+    end
+end
